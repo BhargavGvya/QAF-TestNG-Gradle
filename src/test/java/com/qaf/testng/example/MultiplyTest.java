f2707aa80088bfaa7a02ad352dee9161f22dc73a
BhargavGvya <bhargavgurram@gmail.com>
2021-05-15 22:50:33 +0530

    Initial Repo

---
+++ src/test/java/com/qaf/testng/example/MultiplyTest.java
@@ -0,0 +1,30 @@
+package com.qaf.testng.example;
+
+import org.testng.annotations.Test;
+import org.hamcrest.Matchers;
+import org.testng.Reporter;
+import com.qmetry.qaf.automation.step.QAFTestStep;
+import com.qmetry.qaf.automation.util.Validator;
+
+public class MultiplyTest {
+
+	@QAFTestStep(description = "if numbers are positive")
+	public void checkNumbers()
+	{
+		Reporter.log("This is a dummy test");
+	}
+	
+	@QAFTestStep(description = "multiply numbers")
+	public void multiply()
+	{
+		System.out.println("This is from multiply test");
+		Validator.assertThat(5, Matchers.greaterThan(6));
+	}
+	
+	@Test
+	public void dummyTest()
+	{
+		System.out.println("This test annotated with TestNG class");
+		Reporter.log("Successfully invoked dummy test");
+	}
+}
